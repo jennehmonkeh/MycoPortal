aff88ee475d5f2e70ebcf21bc0156182461ef3fc
Jen <80696369+jennehmonkeh@users.noreply.github.com>
2024-10-02 08:38:43 -0400

    Update Portal.R

--- Portal.R
+++ Portal.R
@@ -5,14 +5,31 @@ library(stringi)
 library(gdata)
 
 #some pre-processing needs to happen in access first - for rust states add to notes
+rm(list = ls())
+# setwd("C:\\DAOM\\MYCOPORTAL\\MYCOPORTAL")
+setwd("C:\\Users\\wilkinsonj\\Documents\\mycoportal")
+daom <- read.csv("DAOMDATA.txt",sep = "\t")
 
-setwd("C:\\DAOM\\DWCA")
-daom <- read.csv("DAOMDATA.TXT",sep = "\t")
+#daom <- read.csv("daom.txt",sep = "\t",header = FALSE, encoding = "UTF-8")
+#names(daom) <- daom[1,]
+#daom <- daom[-1,]
 
 # remove 11k records with no barcode
 daom <- subset(daom, Barcode != "")
 # remove records without identifications
 daom <- subset(daom, GENUS != "")
+daom <- subset(daom, GROUP != "Alga")
+daom <- subset(daom, GROUP != "Bacteria")
+daom <- subset(daom, GROUP != "Insecta")
+daom <- subset(daom, GROUP != "Lichen")
+daom <- subset(daom, GROUP != "Miscellaneous")
+daom <- subset(daom, GROUP != "Nematode")
+daom <- subset(daom, GROUP != "Nomina confusa")
+daom <- subset(daom, GROUP != "Virus")
+
+
+
+daomBACK <- daom
 
 # remove restricted specimens:
 # Verticillium longisporum     
@@ -21,6 +38,11 @@ daom <- subset(daom, DAOM != "550247R")
 daom <- subset(daom, SPECIES != "endobioticum")
 # Alternaria tomatophila 
 daom <- subset(daom, SPECIES != "tomatophila")
+daom <- subset(daom, GENUS != "UNIDENTIFIED")
+
+# don't include authority as MycoPortal autofills as long as taxa in FDex
+#names(daom)<-sub("AUTHORS","scientificNameAuthorship",names(daom))
+
 
 #make a copy of the barcode field for "dbpk" (primary key) needed for MyCoPortal
 daom$occurrenceID <- daom$Barcode
@@ -29,13 +51,13 @@ names(daom)<-sub("Barcode","dbpk",names(daom))
 names(daom)<-sub("DAOM","catalogNumber",names(daom))
 names(daom)<-sub("NOTES","occurrenceRemarks",names(daom))
 names(daom)<-sub("COLLECTOR","recordedBy",names(daom))
-names(daom)<-sub("AUTHORS","scientificNameAuthorship",names(daom))
+
 names(daom)<-sub("ANNOTATED","identificationRemarks",names(daom))
 names(daom)<-sub("VerbatimDate","verbatimEventDate",names(daom))
 names(daom)<-sub("COUNTRY","country",names(daom))
 names(daom)<-sub("COUNTY_TWP","county",names(daom))
 names(daom)<-sub("PROVINCE","stateProvince",names(daom))
-names(daom)<-sub("CITY_ETC","verbatimLocality", names(daom))
+names(daom)<-sub("CITY_ETC","locality", names(daom))
 names(daom)<-sub("ALTITUDE","verbatimElevation",names(daom))
 names(daom)<-sub("LATITUDE","verbatimLatitude",names(daom))
 names(daom)<-sub("LONGITUDE","verbatimLongitude",names(daom))
@@ -51,11 +73,14 @@ names(daom)<-sub("OTHER_NO","otherCatalogNumbers",names(daom))
 names(daom)<-sub("HABITAT","habitat",names(daom))
 names(daom)<-sub("H_ETC","substrate",names(daom))
 
+#build eventDate YYYY-MM-DD using the above 3 columns
 daom$zyear <- daom$year
 daom$zmth <- stri_pad_left(str=daom$month, 2, pad="0")
 daom$zday <- stri_pad_left(str=daom$day, 2, pad="0")
-#build eventDate YYYY-MM-DD using the above 3 columns
-daom <- daom %>% unite("eventDate",year:zday,sep = "-",remove = FALSE,na.rm=TRUE)
+
+daom$basisofrecord <- "PreservedSpecimen"
+
+daom <- daom %>% unite("eventDate",zyear:zday,sep = "-",remove = FALSE,na.rm=TRUE)
 
 # combine columns to new ones
 daom <- daom %>% unite("associatedTaxa", H_GENUS:H_SPEC, sep= " ")
@@ -65,12 +90,16 @@ daom$associatedTaxa <- ifelse(daom$associatedTaxa=="",daom$associatedTaxa,paste(
 
 
 # remove columns
+daom <- subset(daom, select = -c(AUTHORS))
 daom <- subset(daom, select = -c(DATE))
-daom <- subset(daom, select = -c(zday,zmth))
+daom <- subset(daom, select = -c(zyear,zday,zmth))
 daom <- subset(daom, select = -c(GROUP))
-#daom <- subset(daom, select = -c(HostFamily))
+daom <- subset(daom, select = -c(ExsiSetID))
 daom <- subset(daom, select = -c(STATE))
 daom <- subset(daom, select = -c(ANAMORPH))
+daom <- subset(daom, select = -c(X2023Culture, CCFC, FROM, ISOLATE_NO, PrintOrder))
+daom <- subset(daom, select = -c(CULTURE, Image, HostFamily,EXISTS))
+
 
 # create new files, one for upload to canadensys ipt (daom.txt) and one for reference (daom.csv)
 write.table(daom,file = "daom.txt",na = "",quote = FALSE, sep = "\t",row.names = FALSE,fileEncoding = "UTF-8")
